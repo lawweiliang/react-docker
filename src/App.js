7b6aed49555433b3f8721c4b4fc0bba28051644d
liang <yeslaw90@hotmail.com>
2019-12-15 11:34:37 +0800

    modified app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Dude, I am coming come</p>
+        <p>This is feature branch file</p>
         <a
           className="App-link"
           href="https://reactjs.org"
